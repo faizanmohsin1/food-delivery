ea97a39a4a89b5f7c8890232325951d8209d6c96
faizanmohsin1 <81846033+faizanmohsin1@users.noreply.github.com>
2021-08-23 12:04:11 -0700

    Add files via upload

---
+++ RestaurantProfile.js
@@ -0,0 +1,7 @@
+let  editcard  = () => {
+    let resName= document.getElementById("resName");
+    let dishName= document.getElementById("dishName");
+    let price= document.getElementById("price");
+   
+    
+}
